e68b8249a1ff4e9d6ef1a29f5cc837232e58bd2e
PostPunkAesthete <harry_ollI@abv.bg>
2023-05-30 10:18:47 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rock-Paper-Scissors
+ The popular game, written with HTML, CSS & JS.
---
+++ script.js
@@ -0,0 +1,92 @@
+const score = JSON.parse(localStorage.getItem('score')) || {
+  wins: 0,
+  loses: 0,
+  ties: 0
+};
+
+function playGame(playerMove) {
+  let compMove = computerMove();
+
+  let result = '';
+
+  if (playerMove == 'rock') {
+    if (compMove == 'rock') {
+      result = 'Tie.'
+    }
+    else if (compMove == 'paper') {
+      result = 'You lose.'
+    }
+    else {
+      result = 'You win.'
+    }
+  }
+  else if (playerMove == 'paper') {
+    if (compMove == 'paper') {
+      result = 'Tie.'
+    }
+    else if (compMove == 'scissors') {
+      result = 'You lose.'
+    }
+    else {
+      result = 'You win.'
+    }
+  }
+  else {
+    if (compMove == 'scissors') {
+      result = 'Tie.'
+    }
+    else if (compMove == 'rock') {
+      result = 'You lose.'
+    }
+    else {
+      result = 'You win.'
+    }
+  }
+
+  if (result === 'You win.') {
+    score.wins++;
+  }
+  else if (result === 'Tie.') {
+    score.ties++;
+  }
+  else {
+    score.loses++;
+  }
+
+  localStorage.setItem('score', JSON.stringify(score));
+
+  document.querySelector('.js-result').innerHTML=`${result}`;
+  document.querySelector('.js-moves').innerHTML=`You : <img src="images/${playerMove}-emoji.png" class="move-icon"> VS  <img src="images/${compMove}-emoji.png" class="move-icon"> : Computer.`;
+
+  
+  updateScore();
+}
+
+function updateScore() {
+  document.querySelector('.js-score')
+  .innerHTML=`Wins: ${score.wins}, Loses: ${score.loses}, Ties: ${score.ties}.`;
+}
+
+function computerMove() {
+  let randNum = Math.random();
+  let compMove = '';
+
+  if (randNum <= 1 / 3) {
+    compMove = 'rock';
+  }
+  else if (randNum <= 2 / 3) {
+    compMove = 'paper';
+  }
+  else {
+    compMove = 'scissors';
+  }
+  return compMove;
+}
+
+function resetScore() {
+  score.wins = 0;
+  score.loses = 0;
+  score.ties = 0;
+  localStorage.removeItem('score');
+  updateScore();
+}
